20678be5bcd04847c91e51680e45753481bf960c
Severrun <severin96@ya.ru>
2015-05-15 19:54:35 +0300

    some css + роутинг + some html + nginx

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-/**
- * Created by Alex on 27.04.2015.
- */
-var http = require('http'); // РїРѕРґРєР»СЋС‡РµРЅРёРµ РјРѕРґСѓР»СЏ СЂР°Р±РѕС‚С‹ СЃ СЃРµС‚СЊСЋ
-var fs = require('fs'); // РїРѕРґРєР»СЋС‡РµРЅРёРµ РјРѕРґСѓР»СЏ СЂР°Р±РѕС‚С‹ СЃ С„Р°Р№Р»РѕРІРѕР№ СЃРёСЃС‚РµРјРѕР№
+var http = require('http'); // подключение модуля работы с сетью
+var fs = require('fs'); // подключение модуля работы с файловой системой
+var url = require('url');
 
-var server = http.createServer(function(req, res) { // С„СѓРЅРєС†РёСЏ РѕР±СЂР°Р±РѕС‚С‡РёРє HTTP Р·Р°РїСЂРѕСЃРѕРІ. req - HTTP Р·Р°РїСЂРѕСЃ РѕС‚ Р±СЂР°СѓР·РµСЂР°, res - РѕС‚РІРµС‚, С„РѕСЂРјРёСЂСѓРµРјРѕР№ РЅР°С€РµР№ РїСЂРѕРіСЂР°РјРјРѕР№-СЃРµСЂРІРµСЂРѕРј.
-    res.writeHead(200, {'Content-Type': 'text/html'}); // Р”РѕР±Р°РІР»СЏРµС‚ Рє HTTP РѕС‚РІРµС‚Сѓ РєРѕРґ 200 Рё Р·Р°РіРѕР»РѕРІРѕРє Content-type: text/html
-    fs.readFile('index.html', function(err, contents) { // Р§С‚РµРЅРёРµ С„Р°Р№Р»Р° index.html. Р¤СѓРЅРєС†РёСЏ Р±СѓРґРµС‚ РІС‹Р·РІР°РЅР° РїРѕСЃР»Рµ Р·Р°РІРµСЂС€РµРЅРёСЏ СЃС‡РёС‚С‹РІР°РЅРёСЏ. err - РёРЅС„РѕСЂРјР°С†РёСЏ РѕР± РѕС€РёР±РєР°С…, contents - СЃРѕРґРµСЂР¶РёРјРѕРµ, РєРѕС‚РѕСЂРѕРµ СѓРґР°Р»РѕСЃСЊ СЃС‡РёС‚Р°С‚СЊ
-        console.log(err); // Р’С‹РІРѕРґ РІ РєРѕРЅСЃРѕР»СЊ РёРЅС„РѕСЂРјР°С†РёРё РѕР± РѕС€РёР±РєР°С… РїСЂРё С‡С‚РµРЅРёРё С„Р°Р№Р»Р°
-        res.end(contents); // Р—Р°РїРёСЃС‹РІР°РµС‚ РІ С‚РµР»Рѕ HTTP РѕС‚РІРµС‚Р° СЃРѕРґРµСЂР¶РёРјРѕРµ С„Р°Р№Р»Р° index.html Рё РѕС‚РїСЂР°РІР»СЏРµС‚ HTTP РѕС‚РІРµС‚.
+var server = http.createServer(function(req, res) { // функция обработчик HTTP запросов. req - HTTP запрос от браузера, res - ответ, формируемой нашей программой-сервером.
+    var pathname = url.parse(req.url).pathname;
+    if (pathname == "/registration.html")
+    {
+        fs.readFile('registration.html', function (err, contents) { // Чтение файла index.html. Функция будет вызвана после завершения считывания. err - информация об ошибках, contents - содержимое, которое удалось считать
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(contents); 
+        })
+    }
+    if (pathname == "/404.html")
+    {
+        fs.readFile('404.html', function (err, contents) { // Чтение файла index.html. Функция будет вызвана после завершения считывания. err - информация об ошибках, contents - содержимое, которое удалось считать
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(contents);
+        })
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'}); // Добавляет к HTTP ответу код 200 и заголовок Content-type: text/html
+    fs.readFile('index.html', function(err, contents) { // Чтение файла index.html. Функция будет вызвана после завершения считывания. err - информация об ошибках, contents - содержимое, которое удалось считать
+        console.log(err); // Вывод в консоль информации об ошибках при чтении файла
+        res.end(contents); // Записывает в тело HTTP ответа содержимое файла index.html и отправляет HTTP ответ.
     });
 });
-server.listen(8000); // РЅР°С€Р° РїСЂРѕРіСЂР°РјРјР° Р±СѓРґРµС‚ РѕС‚РІРµС‡Р°С‚СЊ РЅР° http Р·Р°РїСЂРѕСЃС‹ РїРѕ Р°РґСЂРµСЃСѓ http://localhost:8000/
+server.listen(8000); // наша программа будет отвечать на http запросы по адресу http://localhost:8000/
 console.log('Server started');
-
-
+<!--Пример комментария-->
